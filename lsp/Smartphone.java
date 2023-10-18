e4345e98df222231b847686424dc961a7b9930cd
saarisdi <riyadi256789@gmail.com>
2023-10-18 07:42:47 +0700

    Menambahkan class Product, Smartphone, Vegetable

---
+++ lsp/Smartphone.java
@@ -0,0 +1,10 @@
+class Smartphone extends Product {
+    @Override
+    String setName() {
+    return "Samsung S10+ Limited Edition";
+    }
+    @Override
+    Date setExpiredDate() {
+    return new Date(); // ???????
+    }
+    }
---
+++ lsp/Vegetable.java
@@ -0,0 +1,10 @@
+class Vegetable extends Product {
+@Override
+String getName() {
+return "Broccoli";
+}
+@Override
+Date getExpiredDate() {
+return new Date();
+}
+}
